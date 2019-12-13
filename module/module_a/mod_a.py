12a724f2356967cda48845a761032e426317c077
Bhawesh Chandola <bhawesh.chandola@gmail.com>
2019-12-13 11:13:10 +0530

    passing environment file while running the test

--- module/module_a/mod_a.py
+++ module/module_a/mod_a.py
@@ -7,9 +7,20 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 sys.path.append(path.dirname( path.abspath(__file__) ))
 from module_b.mod_b import mod_b1
 from config import hel
+import os
+# from dotenv import load_dotenv, find_dotenv
+# from pathlib import Path  # python3 only
+# # print(find_dotenv())
 
+# env_path = '.env'
+# load_dotenv(find_dotenv())
+# print(os.environ['APP_HOST'])
 
 def mod_a1(inp):
     inp1 = mod_b1()
     inp1 += inp 
     return inp1
+
+def mod_a2():
+    print("environ here ",os.environ['APP_HOST'])
+    return os.environ['APP_HOST']
--- module/tests/test_1.py
+++ module/tests/test_1.py
@@ -1,24 +1,40 @@
 import sys
+import os
+
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-from module_a.mod_a import mod_a1
+
+from module_a.mod_a import mod_a1, mod_a2
 import pytest
 import json
 import requests
 
-@pytest.mark.parametrize("num, output",[(1,1),(2,1),(3,2),(4,0)])
-def test_div(num, output):
-    assert mod_a1(num) % 3 == output
-
-def test_div1():
-    assert mod_a1(20) % 6 == 0
+# !pip install pytest-env
 
 
-def test_request_comp_search():
-    data = {
-        "some": "data"
-        }
-    r = requests.post(url, data=json.dumps(data), headers={'content-type': 'application/json'})
-    # assert is_successful(r)
-    data = r.json()
-    assert r.status_code == 200
+print(os.environ['APP_HOST'])
+
+# @pytest.mark.parametrize("num, output",[(1,1),(2,1),(3,2),(4,0)])
+# def test_div(num, output):
+#     assert mod_a1(num) % 3 == output
+
+# def test_div1():
+#     assert mod_a1(20) % 6 == 0
+
+def test_mod2():
+    # os.environ["APP_HOST"] = "local"
+    assert mod_a2() == "local"
+
+def test_mod21():
+    os.environ["APP_HOST"] = "local1"
+    assert mod_a2() == "local1"
+
+
+# def test_request_comp_search():
+#     data = {
+#         "some": "data"
+#         }
+#     r = requests.post(url, data=json.dumps(data), headers={'content-type': 'application/json'})
+#     # assert is_successful(r)
+#     data = r.json()
+#     assert r.status_code == 200
